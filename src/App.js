d2adf905dfef515986cb262b6bec03ec8ece45e6
adeojo emmanuel <emmanuel.adeojo.ibk@gmail.com>
2023-12-06 09:30:49 +0100

    feat: implement react connect button

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [connected, setConnected] = useState(false);
+
+  const handleConnect = async () => {
+    try {
+      // Load the Verified SDK from CDN
+      const sdkScript = document.createElement('script');
+      sdkScript.src = 'https://cdn.verified.network/js/verified-sdk.js';
+      sdkScript.async = true;
+      document.head.appendChild(sdkScript);
+
+      // Wait for the script to load
+      await new Promise(resolve => {
+        sdkScript.onload = resolve;
+      });
+
+      // Initialize the SDK
+      window.VerifiedSdk.initialize();
+
+      // Connect using the SDK
+      await window.VerifiedSdk.connect();
+
+      // Update the state to indicate successful connection
+      setConnected(true);
+    } catch (error) {
+      console.error('Error connecting:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleConnect} disabled={connected}>
+          {connected ? 'Connected' : 'Connect'}
+        </button>
       </header>
     </div>
   );
